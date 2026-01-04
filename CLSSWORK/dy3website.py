60c4bc529717005d94b734daab41a0438f5e258b
Haleema fm <haleemafm88@gmail.com>
2026-01-04 22:53:09 +0400

    classwork

--- CLSSWORK/dy3website.py
+++ CLSSWORK/dy3website.py
@@ -6,8 +6,8 @@ is_email_valid="@" in email
 user_age=17
 is_age_valid=user_age >= 18
 can_login_final=has_account and email_verified and is_email_valid and is_age_valid
-print("can login:",can_login)
-print("is the user email valid :",is_email_valid)
+print("Can login:",can_login)
+print("Is the user email valid :",is_email_valid)
 print("Is the user age valid:",is_age_valid )
-print("at last user can log in or not:",can_login_final)
-print("has account is:",has_account is True)
+print("At last user can log in or not:",can_login_final)
+print("Has account is:",has_account is True)
